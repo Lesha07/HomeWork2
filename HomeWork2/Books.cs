b30cf41b21d4585844775fa43a0d86429aad6648
Lesha07 <simchuklesha@gmail.com>
2024-12-21 12:09:55 +0200

    1

--- HomeWork2/Books.cs
+++ HomeWork2/Books.cs
@@ -1,6 +1,93 @@
 namespace HomeWork2;
 
-public class Books
+public class Book
 {
+    public string NameBook { get; set; }
+    public string AuthorBook { get; set; }
+    public string PublisherBook { get; set; }
     
+    public Book(string nameBook, string authorBook, string publisherBook)
+    {
+        NameBook = nameBook;
+        AuthorBook = authorBook;
+        PublisherBook = publisherBook;
+    }
+
+    public override string ToString()
+    {
+        return $"Название: {NameBook}, Автор: {AuthorBook}, Издательство: {PublisherBook}";
+    }
+}
+
+public class Libary
+{
+
+    public List<Book> books { get; set; } = new List<Book>();
+
+    public void AddBooks(Book book)
+    {
+        books.Add(book);
+    }
+
+    public void RemoveBooks(string nameBook)
+    { 
+        var bookToRemove = books.FirstOrDefault(x => x.NameBook.Equals(nameBook, StringComparison.OrdinalIgnoreCase));
+        
+        if (bookToRemove != null) 
+        {
+            books.Remove(bookToRemove);
+           
+            Console.WriteLine($"Вы успешно взяли книгу {nameBook}");
+        }
+        else
+        { 
+            Console.WriteLine($"Упс, не удалось найти книгу {nameBook} :(");
+        }
+    }
+
+    public void FindBooks(string query)
+    {
+        var findBooks = books.FindAll(b =>
+            b.NameBook.Contains(query, StringComparison.OrdinalIgnoreCase) ||
+            b.AuthorBook.Contains(query, StringComparison.OrdinalIgnoreCase) ||
+            (b is Book book && 
+             book.PublisherBook.Contains(query, StringComparison.OrdinalIgnoreCase))
+        );
+
+        if (findBooks.Count > 0)
+        {
+            foreach (var book in findBooks)
+            {
+                Console.WriteLine($"Книга {book} упешно найдена!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Соре такой книги нема");
+        }
+    }
+    public void UpdatePublisher(string nameBook, string newPublisher) 
+    { 
+        var bookToUpdate = books.Find(x => x.NameBook.Equals(nameBook, StringComparison.OrdinalIgnoreCase));
+        
+        if (bookToUpdate != null) 
+        { 
+            bookToUpdate.PublisherBook = newPublisher; 
+            Console.WriteLine($"Издательство книги \"{nameBook}\" успешно изменено на \"{newPublisher}\"!"); 
+        }
+        else 
+        { 
+            Console.WriteLine("Упс, книгу не удалось найти.");
+        }
+    }
+       
+    public void DisplayBooks()
+    {
+        Console.WriteLine("Список всех книг в библиотеке:");
+
+        foreach (var book in books)
+        {
+            Console.WriteLine(book);
+        }
+    }
 }
--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -1,2 +1,101 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HomeWork2;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Добро пожаловать в библиотеку!!");
+        
+        Libary libary = new Libary();
+        
+        libary.AddBooks(new Book("Мастер и Маргарита","Булгаков", "АДЕФ-Украина") );
+        
+        libary.AddBooks(new Book("Игра престолов", "Джордж Р. Р. Мартин", 
+            "Bantam Spectra Voyager Books"));
+        
+        bool ProgramRun = true;
+
+        while (ProgramRun)
+        {
+            Console.WriteLine("\n0. Посмотреть все книги " +
+                              "\n1. Добавить книгу в библиотеку. " +
+                              "\n2. Взять книгу из библиотеки. " +
+                              "\n3. Поиск книги по автору, названию или изданию. " +
+                              "\n4. Переименновать издание." +
+                              "\n5. Закрыть библиотеку");
+
+            Console.WriteLine("Выберите действие:");
+            
+            string? userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "0":
+                    
+                    Console.Clear();
+                    
+                    libary.DisplayBooks();
+                    break;
+                
+                case "1":
+                    
+                    Console.Clear();
+                    
+                    Console.WriteLine("Введите название книги: ");
+                    string? nameBook = Console.ReadLine();
+
+                    Console.WriteLine("Введите имя автора: ");
+                    string? authorBook = Console.ReadLine();
+
+                    Console.WriteLine("Введите издателя: ");
+                    string? publisherBook = Console.ReadLine();
+                    
+                    Book newBook = new Book(nameBook, authorBook, publisherBook);
+                    libary.AddBooks(newBook);
+                    
+                    Console.WriteLine("Книга успешно добавлена!");
+                    break;
+                    
+                case "2":
+                    Console.Clear();
+                    
+                    Console.WriteLine("Выберие книгу которую вы хотите взять:");
+                    string? bookToRemove = Console.ReadLine();
+                    
+                    libary.RemoveBooks(bookToRemove);
+                    break;
+                
+                case "3":
+                    
+                    Console.Clear();
+                    
+                    Console.Write("Введите название книги, автора или издательство для поиска: ");
+                    string query = Console.ReadLine();
+                    
+                    libary.FindBooks(query);
+                    break;
+                   
+                case "4":
+                   
+                    Console.Clear();
+                    
+                    Console.WriteLine("Введетие название книги, издание которой вы хотите изменить: ");
+                    string NameBook = Console.ReadLine();
+                    
+                    Console.WriteLine("Введите название нвого издательства: "); 
+                    string newPublisher = Console.ReadLine();
+
+                    libary.UpdatePublisher(NameBook, newPublisher);
+                    
+                    break;
+                
+                case "5":
+                    ProgramRun = false;
+                    Console.WriteLine("Выходим.....");
+                    break;
+            }
+            
+            Console.WriteLine("Спасибо за выбор нашей библиотки, ждём вас снова. Пока!");
+        }
+    }
+}
